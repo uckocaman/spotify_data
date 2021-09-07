556b8aa0f156baf25f16540dc31c892f58cd9a9a
Umut Can KOCAMAN <33186542+uckocaman@users.noreply.github.com>
2021-09-07 17:53:32 +0300

    Create create_table.sql
    Creating the database table to load the playlist data.

---
+++ Playlist Scripts/create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE playlists
+(
+        playlist_name VARCHAR(200),
+        lastModifiedDate DATE,
+        trackName VARCHAR(200),
+        artistName VARCHAR(200),
+        albumName VARCHAR(200)
+)
